c0e5dba03954fca5e92fac858194b4e74f3bb510
Clydinite <clydinite@gmail.com>
2026-06-29 18:49:38 +0800

    fixed <pattern> in sample flashcard

--- preview.py
+++ preview.py
@@ -22,7 +22,7 @@ sample_card = Flashcard.model_validate(
                                 "translation": "雷達偵測到一個奇特、不明的物體在海面上以令人難以置信的速度移動。",
                             },
                             {
-                                "sentence": "The museum display features a collection of everyday <target>object</target>s from the ancient Roman era.",
+                                "sentence": "The museum display features a collection of everyday <target>objects</target> from the ancient Roman era.",
                                 "translation": "博物館的展覽展示了一系列來自古羅馬時期的日常物品。",
                             },
                         ],
